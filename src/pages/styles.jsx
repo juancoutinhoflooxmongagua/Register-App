792888b0e01f3e2bfd6346575642adb3f0544229
juancoutinhoflooxmongagua <juan.coutinho.flooxmongagua@gmail.com>
2025-04-01 11:52:18 -0300

    feat: add modularize styles

---
+++ src/pages/styles.jsx
@@ -0,0 +1,11 @@
+import React from "react"
+import './styles.css'
+
+export default function Styles() {
+    return (
+        <div>
+            <h1>Vários estilos diferentes</h1>
+            <p>eu sou um subtitulo</p>
+        </div>
+    )
+}
