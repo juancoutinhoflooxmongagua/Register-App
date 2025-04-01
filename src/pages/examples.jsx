fe8062c2ba8ec3c70c853e57f893bee4bdd9246c
juancoutinhoflooxmongagua <juan.coutinho.flooxmongagua@gmail.com>
2025-04-01 12:07:15 -0300

    feat: finish component layout

--- src/pages/examples.jsx
+++ src/pages/examples.jsx
@@ -1,10 +1,13 @@
 import Header from "@/components/Header";
+import Layout from "@/components/Layout";
 
 export default function Examples (){
     return (
+        <Layout>
         <div>
         <Header titulo="oi eu sou um titulo"></Header>
         <h1>exemplo</h1>
         </div>
+        </Layout>
     )
 }
--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -1,10 +1,12 @@
 import Link from "next/link";
-
+import Layout from "@/components/Layout";
 export default function Teste() {
     return (
+        <Layout>
         <div>
             <Link href="/examples">Exemplos</Link>
             <h1>Olá</h1>
         </div>
+        </Layout>
     );
 }
