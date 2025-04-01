e280ef5ba747d64e564e7311189734f04ab029ad
juancoutinhoflooxmongagua <juan.coutinho.flooxmongagua@gmail.com>
2025-04-01 11:56:01 -0300

    feat: working with Components

---
+++ src/components/Header.jsx
@@ -0,0 +1,7 @@
+export default function Header(){
+    return (
+        <header>
+            <h2>Fundamentos de next js</h2>
+        </header>
+    )
+}
---
+++ src/pages/examples.jsx
@@ -0,0 +1,10 @@
+import Header from "@/components/Header";
+
+export default function Examples (){
+    return (
+        <div>
+        <Header></Header>
+        <h1>exemplo</h1>
+        </div>
+    )
+}
